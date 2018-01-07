a6b02d09d0c44cdadbdd068bc6688dbd59c6ccd1
codenotfound <codenotfound.com@gmail.com>
2018-01-07 12:17:16 +0100

    add cxf-jaxws-jetty-logging-log4j

---
+++ cxf-jaxws-jetty-logging-log4j/README.md
@@ -0,0 +1,7 @@
+# cxf-jaxws-jetty-logging-log4j
+
+[![Quality Gate](https://sonarcloud.io/api/badges/gate?key=com.codenotfound:cxf-jaxws-jetty-logging-log4j)](https://sonarcloud.io/dashboard/index/com.codenotfound:cxf-jaxws-jetty-logging-log4j)
+
+A code sample which shows how to configure CXF to log the request and response SOAP messages using Log4j.
+
+[https://www.codenotfound.com/jaxws-cxf-logging-request-response-soap-messages-log4j.html](https://www.codenotfound.com/jaxws-cxf-logging-request-response-soap-messages-log4j.html)
---
+++ cxf-jaxws-jetty-logging-log4j/src/test/java/com/codenofound/soap/http/cxf/HelloWorldImplIT.java
@@ -0,0 +1,31 @@
+package com.codenofound.soap.http.cxf;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.codenotfound.services.helloworld.Person;
+import com.codenotfound.soap.http.cxf.HelloWorldClient;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    locations = {"classpath:/META-INF/spring/context-requester.xml"})
+public class HelloWorldImplIT {
+
+  @Autowired
+  private HelloWorldClient helloWorldClient;
+
+  @Test
+  public void testSayHello() {
+    Person person = new Person();
+    person.setFirstName("John");
+    person.setLastName("Doe");
+
+    assertEquals("Hello John Doe!",
+        helloWorldClient.sayHello(person));
+  }
+}
